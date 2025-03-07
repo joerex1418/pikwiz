0f55d3e87814ed8cb493655d13b827897f7865d2
Joe <67286232+joerex1418@users.noreply.github.com>
2025-03-07 15:03:46 -0600

    Update prompt-manipulation.js

--- app/static/prompt-manipulation.js
+++ app/static/prompt-manipulation.js
@@ -3,7 +3,7 @@ const negativePromptElem = document.querySelector("#negative-prompt textarea");
 
 positivePromptElem.addEventListener("keydown", function(e) {
     // if (e.ctrlKey & e.shiftKey) {
-    if ((e.ctrlKey & e.shiftKey) | (e.shiftKey)) {
+    if ((e.ctrlKey & e.shiftKey)) {
         switch (e.key) {
             case "ArrowLeft":
                 moveTagLeft(positivePromptElem); break;
@@ -20,7 +20,7 @@ positivePromptElem.addEventListener("keydown", function(e) {
 
 negativePromptElem.addEventListener("keydown", function(e) {
     // if (e.ctrlKey & e.shiftKey) {
-    if ((e.ctrlKey & e.shiftKey) | (e.shiftKey)) {
+    if ((e.ctrlKey & e.shiftKey)) {
         switch (e.key) {
             case "ArrowLeft":
                 moveTagLeft(negativePromptElem); break;
@@ -42,17 +42,43 @@ negativePromptElem.addEventListener("keydown", function(e) {
 function moveTagLeft(promptElem) {
     let selectionStart = promptElem.selectionStart;
 
-    if (selectionStart) {
+    if (selectionStart >=0) {
+        let tagList = promptElem.value.split(",");
+        /* Check if less than 3 tags */
+        if (tagList.length < 3) {
+            if (tagList.length == 2) {
+                let selectedTagSide = promptElem.value.slice(selectionStart).indexOf(",") == -1 ? "right" : "left"
+                
+                let selectedTagString = tagList[0].trim();
+                let secondaryTagString = tagList[1].trim();
+                
+                if (selectedTagSide == "right") {
+                    selectedTagString = tagList[1].trim();
+                    secondaryTagString = tagList[0].trim();
+                }
+
+                // let newString = secondaryTagString + ", " + selectedTagString;
+                let newString = selectedTagString + ", " + secondaryTagString;
+
+                let newSelectedTagStartIndex = newString.indexOf(selectedTagString);
+                let newSelectedTagEndIndex = newSelectedTagStartIndex + selectedTagString.length;
+                
+                promptElem.value = newString;
+                promptElem.setSelectionRange(newSelectedTagStartIndex, newSelectedTagEndIndex);
+            } else {
+                promptElem.setSelectionRange(0, promptElem.value.length)
+            }
+        }
         /* Check if selected tag is the LEFTMOST tag */
-        if (promptElem.value.slice(0, selectionStart).indexOf(",") == -1) {
-            promptElem.setSelectionRange(0, promptElem.value.indexOf(","));
+        else if (promptElem.value.slice(0, selectionStart).indexOf(",") == -1) {
             // No need to do anything
-            console.log("No movement needed")
+            // console.log("No movement needed")
+            promptElem.setSelectionRange(0, promptElem.value.indexOf(","));
             return;
         }
 
         /* Check if selected tag is immediately to the RIGHT of the LEFTMOST tag */
-        if ( (promptElem.value.slice(0, selectionStart).split(",").length - 1) == 1 ) {
+        else if ( (promptElem.value.slice(0, selectionStart).split(",").length - 1) == 1 ) {
             let endIndex = promptElem.value.indexOf(",", selectionStart) - 1;
             let startIndex = promptElem.value.slice(0, endIndex + 1).lastIndexOf(",") + 1;
             let primaryTagString = promptElem.value.slice(startIndex, endIndex + 1).trim()
@@ -119,34 +145,83 @@ function moveTagLeft(promptElem) {
 function moveTagRight(promptElem) {
     let selectionStart = promptElem.selectionStart;
 
-    if (selectionStart) {
+    if (selectionStart >= 0) {
+        let tagList = promptElem.value.split(",")
+        /* Check if less than 3 tags */
+        if (tagList.length < 3) {
+            if (tagList.length == 2) {
+                let primaryTagSide = promptElem.value.slice(selectionStart).indexOf(",") == -1 ? "right" : "left"
+                
+                let primaryTagString = tagList[0].trim();
+                let secondaryTagString = tagList[1].trim();
+                
+                if (primaryTagSide == "right") {
+                    primaryTagString = tagList[1].trim();
+                    secondaryTagString = tagList[0].trim();
+                }
+
+                let newString = secondaryTagString + ", " + primaryTagString;
+
+                let newPrimaryTagStartIndex = newString.indexOf(primaryTagString);
+                let newPrimaryTagEndIndex = newPrimaryTagStartIndex + primaryTagString.length;
+                
+                promptElem.value = newString;
+                promptElem.setSelectionRange(newPrimaryTagStartIndex, newPrimaryTagEndIndex);
+            } else {
+                promptElem.setSelectionRange(0, promptElem.value.length)
+            }
+        }
         /* Check if selected tag is the RIGHTMOST tag */
-        if (promptElem.value.slice(selectionStart).indexOf(",") == -1) {
-            promptElem.setSelectionRange(promptElem.value.lastIndexOf(",") + 1, promptElem.value.length);
+        else if (promptElem.value.slice(selectionStart).indexOf(",") == -1) {
             // No need to do anything
-            console.log("No movement needed")
-            return;
+            // console.log("Selected TAG is RIGHTMOST tag")
+            promptElem.setSelectionRange(promptElem.value.lastIndexOf(",") + 1, promptElem.value.length);
         }
         /* Check if selected tag is immediately to the LEFT of the RIGHTMOST tag */
-        // ...wait, lol. This is only needs to be checked in the 'moveTagRight()' function
-        
-        // else if ( (promptElem.value.slice(selectionStart).split(",").length - 1) == 1 ) {
-        //     let endIndex = promptElem.value.slice(selectionStart).indexOf(",") - 1;
-        //     let startIndex = promptElem.value.slice(0, selectionStart).lastIndexOf(",") + 1;
-        //     let primaryTagString = promptElem.value.slice(startIndex, endIndex + 1);
+        else if ( (promptElem.value.slice(selectionStart).split(",").length - 1) == 1 ) {
+            // console.log("Selected TAG is immediately to the LEFT of the RIGHTMOST tag")
+            let endIndex = promptElem.value.indexOf(",", selectionStart) - 1;
+            let startIndex = promptElem.value.slice(0, selectionStart).lastIndexOf(",") + 1;
+            let primaryTagString = promptElem.value.slice(startIndex, endIndex + 1).trim();
 
-        // } 
-        else {
-            // let endCommaIndex = promptElem.value.indexOf(",", selectionStart);
-            // let startCommaIndex = promptElem.value.slice(0, endCommaIndex).lastIndexOf(",");
-            // let primaryTagString = promptElem.value.slice(startCommaIndex + 1, endCommaIndex).trim()
+            let rightTagStartIndex = promptElem.value.lastIndexOf(",") + 1;
+            let rightTagString = promptElem.value.slice(rightTagStartIndex).trim();
+
+            let untouchedLeftSide = promptElem.value.slice(0, startIndex - 1).trim();
+
+            let newString = untouchedLeftSide + ", " + rightTagString + ", " + primaryTagString;
+
+            let newPrimaryTagStartIndex = newString.indexOf(primaryTagString);
+            let newPrimaryTagEndIndex = newPrimaryTagStartIndex + primaryTagString.length;
             
-            // let rightTagEndCommaIndex = promptElem.value.slice(endCommaIndex + 1).indexOf(",") + endCommaIndex + 1
-            // let rightTagString = promptElem.value.slice(endCommaIndex + 1, rightTagEndCommaIndex).trim()
+            promptElem.value = newString;
     
-            // let untouchedLeftSide = promptElem.value.slice(0, startCommaIndex).trim()
-            // let untouchedRightSide = promptElem.value.slice(rightTagEndCommaIndex + 1).trim()
+            promptElem.setSelectionRange(newPrimaryTagStartIndex, newPrimaryTagEndIndex);
+
+        } 
+        /* Check if selected tag is the LEFTMOST tag */
+        else if ( (promptElem.value.slice(0, selectionStart).indexOf(",") == -1) || (selectionStart == 0)) {
+            // console.log("Selected TAG is the LEFTMOST tag")
+            let endIndex = promptElem.value.indexOf(",") - 1;
+            let primaryTagString = promptElem.value.slice(0, endIndex + 1).trim();
+
+            let rightTagStartIndex = endIndex + 2;
+            let rightTagEndIndex = promptElem.value.indexOf(",", rightTagStartIndex) - 1;
+            let rightTagString = promptElem.value.slice(rightTagStartIndex, rightTagEndIndex + 1).trim();
+
+            let untouchedRightSide = promptElem.value.slice(rightTagEndIndex + 2).trim();
+
+            let newString = rightTagString + ", " + primaryTagString + ", " + untouchedRightSide;
             
+            let newPrimaryTagStartIndex = newString.indexOf(primaryTagString);
+            let newPrimaryTagEndIndex = newPrimaryTagStartIndex + primaryTagString.length;
+            
+            promptElem.value = newString;
+    
+            promptElem.setSelectionRange(newPrimaryTagStartIndex, newPrimaryTagEndIndex);   
+        }
+        else {
+            // console.log("Selected TAG is surrounded by other tags")
             let endIndex = promptElem.value.indexOf(",", selectionStart) - 1;
             let startIndex = promptElem.value.slice(0, endIndex + 1).lastIndexOf(",") + 1;
             let primaryTagString = promptElem.value.slice(startIndex, endIndex + 1).trim()
@@ -157,13 +232,6 @@ function moveTagRight(promptElem) {
     
             let untouchedLeftSide = promptElem.value.slice(0, startIndex - 1).trim()
             let untouchedRightSide = promptElem.value.slice(rightTagEndIndex + 2).trim()
-
-            console.log("untouchedLeftSide:\t", untouchedLeftSide)
-            console.log("rightTagString:\t", rightTagString)
-            console.log("rightTagStartIndex:\t", rightTagStartIndex)
-            console.log("rightTagEndIndex:\t", rightTagEndIndex)
-            console.log("primaryTagString:\t", primaryTagString)
-            console.log("untouchedRightSide:\t", untouchedRightSide)
     
             let newString = untouchedLeftSide + ", " + rightTagString + ", " + primaryTagString + ", " + untouchedRightSide;
             
